e10ecf1670fd8999589faf4081824ccd9697d5cd
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-05-02 19:23:45 +0530

    layout and parse server output working as expected

---
+++ lib/ApplicationConstants.dart
@@ -0,0 +1,5 @@
+abstract class ApplicationConstants {
+  static const String keyParseApplicationId = "4084ec34-bcea-45f1-a640-16163834eddd";
+  static const String keyParseCustomerKey = "gBgp9vAbqZm67sN6YPruYAYgnnU2Larr";
+  static const String keyParseServerUrl = "https://parse.buddy.com/parse";
+}
--- lib/productscreen.dart
+++ lib/productscreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hello_world/main.dart';
 import 'package:hello_world/layouts.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'ApplicationConstants.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
 class productscreen extends StatefulWidget {
   @override
@@ -20,21 +22,73 @@ class _productscreenState extends State<productscreen> {
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: new Container(
-          child: new Row(
+          child: new Column(
             children: <Widget>[
-              Expanded(
-                child: RaisedButton(
-                  onPressed: () {
-                    Fluttertoast.showToast(msg: 'Add New Product');
-                    Navigator.pushNamed(context, '/addproductscreen');
-                  },
-                  child: Text('Add New Product'),
-                ),
+              RaisedButton(
+                onPressed: () {
+                  Fluttertoast.showToast(msg: 'Add New Product');
+                  Navigator.pushNamed(context, '/addproductscreen');
+                },
+                child: Text('Add New Product'),
               ),
+              Expanded(child:
+              new FutureBuilder(
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return Container(
+                      child: Center(
+                        child: Text("Loading..."),
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemBuilder: (BuildContext context, int index) {
+                        return new ListTile(
+                        );
+                      },
+                      itemCount: snapshot.data.length,
+                    );
+                  }
+                },
+                future: query(),
+              ),),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  initState(){
+    initParse();
+    super.initState();
+  }
+
+  initParse() async {
+    Parse().initialize(ApplicationConstants.keyParseApplicationId,
+        ApplicationConstants.keyParseServerUrl,
+        masterKey: ApplicationConstants.keyParseCustomerKey,
+        clientKey: ApplicationConstants.keyParseCustomerKey,
+        debug: true);
+    var response = await Parse().healthCheck();
+    if (response.success) {
+      print("Success");
+    } else {
+      print("Server health check failed");
+    }
+  }
+
+  query() async{
+    QueryBuilder<ParseObject> queryBuilder = QueryBuilder<ParseObject>(ParseObject('ProductDatabase'));
+
+    ParseResponse apiResponse = await queryBuilder.query();
+    if (apiResponse.success && apiResponse.result != null) {
+      final List<ParseObject> listFromApi = apiResponse.result;
+      final ParseObject parseObject = listFromApi?.first;
+      print(parseObject.type.toString());
+    } else {
+      print('Result: ${apiResponse.error.message}');
+    }
   }
 }
