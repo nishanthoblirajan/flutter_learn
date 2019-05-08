b82e62f4e2457285eb1ac523ef207f4504023fd1
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-05-08 19:53:40 +0530

    futurebuilder working as expected

--- lib/CategoryScreen.dart
+++ lib/CategoryScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:hello_world/AddCategoryScreen.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
@@ -17,17 +18,18 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-
   String categoryType;
 
   TextEditingController searchTextController = new TextEditingController();
 
+
+
+  /*Use normal listview after setting the query in initState*/
   @override
   initState() {
     categoryType = widget.categoryType;
     searchTextController.text = "";
     initSharedPrefs();
-    query(roCode, categoryType, searchTextController.text);
     super.initState();
   }
 
@@ -41,46 +43,71 @@ class _CategoryScreenState extends State<CategoryScreen> {
     });
   }
 
+
+  List<Widget> _getData(AsyncSnapshot snapshot) {
+    ParseResponse apiResponse = snapshot.data;
+    if (apiResponse.success && apiResponse.result != null) {
+      final List<ParseObject> listFromApi = apiResponse.result;
+        _categoryDatabase = new List();
+      for (int i = 0; i < listFromApi.length; i++) {
+        Map output = json.decode(listFromApi[i].toString());
+        CategoryDatabase categoryDatabase =
+        new CategoryDatabase().fromJson(output);
+        print(categoryDatabase.category_name);
+          _categoryDatabase.add(categoryDatabase);
+      }
+    }
+
+    List<Widget> widgetLists = new List();
+    for (int index = 0; index < _categoryDatabase.length; index++) {
+      widgetLists.add(ListTile(
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () {
+            setState(() {
+              deleteCategoryFromDatabase(_categoryDatabase[index]);
+            });
+          },
+        ),
+        title: Text(_categoryDatabase[index].category_name),
+        onTap: () {
+          sharedPreferences.setString(categoryType + "_category",
+              _categoryDatabase[index].category_name);
+          Navigator.of(context).pop({
+            'category_selection':
+            _categoryDatabase[index].category_name
+          });
+        },
+      ));
+    }
+    return widgetLists;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    var loading = CircularProgressIndicator();
+
 
     var futurebuilder = new FutureBuilder(
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        print('Connection State: '+snapshot.connectionState.toString());
-            if (snapshot.hasError)
-              return new Text('Error: ${snapshot.error}');
-            else {
-              return ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return new ListTile(
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        setState(() {
-                          deleteCategoryFromDatabase(_categoryDatabase[index]);
-                        setState(() {
-                          new CategoryScreen(categoryType: categoryType);
-                        });
-                        });
-                      },
-                    ),
-                    title: Text(_categoryDatabase[index].category_name),
-                    onTap: () {
-                      sharedPreferences.setString(categoryType + "_category",
-                          _categoryDatabase[index].category_name);
-                      Navigator.of(context).pop({
-                        'category_selection': _categoryDatabase[index].category_name
-                      });
-                    },
-                  );
-                },
-                itemCount: snapshot.data.length,
-              );
-
-            }
+        print('Connection State: ' + snapshot.connectionState.toString());
+        if(snapshot.hasData){
+          if(snapshot.data!=null){
+            print('Im Inside');
+            return ListView(children:
+            _getData(snapshot));
+          }else{
+            return Center(
+              child: new CircularProgressIndicator(),
+            );
+          }
+        }else{
+          return Center(
+            child: new CircularProgressIndicator(),
+          );
+        }
       },
-      future: query(roCode, categoryType, searchTextController.text),
+      future: _query(roCode, categoryType, searchTextController.text),
     );
 
     return Scaffold(
@@ -88,7 +115,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
         onPressed: () async {
           /*TODO add new category name*/
           Navigator.of(context).push(new MaterialPageRoute(
-              builder: (context) => AddCategoryScreen(
+              builder: (context) =>
+                  AddCategoryScreen(
                     categoryType: 'Product',
                     categoryList: _categoryDatabase,
                   )));
@@ -114,52 +142,55 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   List<CategoryDatabase> _categoryDatabase = [];
 
-  query(String roCode, String categoryType, String textSearch) async {
+  _query(String roCode, String categoryType, String textSearch) async {
     QueryBuilder<ParseObject> queryBuilder;
     if (textSearch == "") {
       queryBuilder = QueryBuilder<CategoryDatabase>(CategoryDatabase())
-        ..whereEqualTo(CategoryDatabase.roCode, roCode)
-        ..whereEqualTo(CategoryDatabase.categoryType, categoryType);
+        ..whereEqualTo(CategoryDatabase.roCode, roCode)..whereEqualTo(
+            CategoryDatabase.categoryType, categoryType);
     } else {
       queryBuilder = QueryBuilder<CategoryDatabase>(CategoryDatabase())
-        ..whereEqualTo(CategoryDatabase.roCode, roCode)
-        ..whereEqualTo(CategoryDatabase.categoryType, categoryType)
-        ..whereEqualTo(CategoryDatabase.categoryName, textSearch);
+        ..whereEqualTo(CategoryDatabase.roCode, roCode)..whereEqualTo(
+            CategoryDatabase.categoryType, categoryType)..whereEqualTo(
+            CategoryDatabase.categoryName, textSearch);
     }
+    return queryBuilder.query();
 
-    ParseResponse apiResponse = await queryBuilder.query();
-    if (apiResponse.success && apiResponse.result != null) {
-      final List<ParseObject> listFromApi = apiResponse.result;
-      setState(() {
-        _categoryDatabase = new List();
-      });
-      for (int i = 0; i < listFromApi.length; i++) {
-        Map output = json.decode(listFromApi[i].toString());
-        CategoryDatabase categoryDatabase =
-            new CategoryDatabase().fromJson(output);
-        print(categoryDatabase.category_name);
-        setState(() {
-          _categoryDatabase.add(categoryDatabase);
-        });
-      }
-      return _categoryDatabase;
-    } else {}
+
+//    ParseResponse apiResponse = await queryBuilder.query();
+//    if (apiResponse.success && apiResponse.result != null) {
+//      final List<ParseObject> listFromApi = apiResponse.result;
+//      setState(() {
+//        _categoryDatabase = new List();
+//      });
+//      for (int i = 0; i < listFromApi.length; i++) {
+//        Map output = json.decode(listFromApi[i].toString());
+//        CategoryDatabase categoryDatabase =
+//        new CategoryDatabase().fromJson(output);
+//        print(categoryDatabase.category_name);
+//        setState(() {
+//          _categoryDatabase.add(categoryDatabase);
+//        });
+//      }
+//      return _categoryDatabase;
+//    } else {}
   }
 
-  /*Delete*/
-  Future deleteCategoryFromDatabase(
-      CategoryDatabase receivedCategoryDatabase) async {
-    String objectID = receivedCategoryDatabase.get('objectId');
-    print('Delete ' + objectID);
-    CategoryDatabase categoryDatabase = new CategoryDatabase();
-    categoryDatabase.set('objectId', objectID);
-    print('Delete ' + categoryDatabase.toString());
-    var deleteResponse = await categoryDatabase.delete();
-    if (deleteResponse.success) {
-      Fluttertoast.showToast(
-          msg: 'Product Deleted', backgroundColor: Colors.blue);
-    } else {
-      Fluttertoast.showToast(msg: 'Error', backgroundColor: Colors.red);
-    }
+}
+
+/*Delete*/
+Future deleteCategoryFromDatabase(
+    CategoryDatabase receivedCategoryDatabase) async {
+  String objectID = receivedCategoryDatabase.get('objectId');
+  print('Delete ' + objectID);
+  CategoryDatabase categoryDatabase = new CategoryDatabase();
+  categoryDatabase.set('objectId', objectID);
+  print('Delete ' + categoryDatabase.toString());
+  var deleteResponse = await categoryDatabase.delete();
+  if (deleteResponse.success) {
+    Fluttertoast.showToast(
+        msg: 'Product Deleted', backgroundColor: Colors.blue);
+  } else {
+    Fluttertoast.showToast(msg: 'Error', backgroundColor: Colors.red);
   }
 }
