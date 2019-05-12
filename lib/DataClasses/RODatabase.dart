65be0ba87867012ecfa0bdb69648d20d8d18f339
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-05-12 15:40:53 +0530

    getting invoice number for RODatabase working

--- lib/DataClasses/RODatabase.dart
+++ lib/DataClasses/RODatabase.dart
@@ -1,28 +1,35 @@
 
-
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
-class RODatabase extends ParseObject implements ParseCloneable{
+class RODatabase extends ParseObject implements ParseCloneable {
   static const String _keyTableName = 'RODatabase';
 
   RODatabase() : super(_keyTableName);
-  RODatabase.clone(): this();
 
-  @override clone(Map map) => RODatabase.clone()..fromJson(map);
+  RODatabase.clone() : this();
+
+  @override
+  clone(Map map) => RODatabase.clone()..fromJson(map);
 
   static const String roCode = 'ro_code';
+
   String get ro_code => get<String>(roCode);
+
   set ro_code(String ro_code) => set<String>(roCode, ro_code);
 
   static const String invoiceNumber = 'invoice_number';
-  String get invoice_number => get<String>(invoiceNumber);
-  set invoice_number(String invoice_number) => set<String>(invoiceNumber, invoice_number);
 
+  String get invoice_number => get<String>(invoiceNumber);
+
+  set invoice_number(String invoice_number) =>
+      set<String>(invoiceNumber, invoice_number);
 
   static const String purchaseNumber = 'purchase_number';
-  String get purchase_number => get<String>(purchaseNumber);
-  set purchase_number(String purchase_number) => set<String>(purchaseNumber, purchase_number);
 
+  String get purchase_number => get<String>(purchaseNumber);
+
+  set purchase_number(String purchase_number) =>
+      set<String>(purchaseNumber, purchase_number);
 
 
 }
--- lib/Forms/InvoiceForm.dart
+++ lib/Forms/InvoiceForm.dart
@@ -1,37 +1,62 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hello_world/ContactScreen.dart';
 import 'package:hello_world/DataClasses/RODatabase.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class InvoiceForm extends StatefulWidget {
   @override
   _InvoiceFormState createState() => _InvoiceFormState();
 }
 
-
 class _InvoiceFormState extends State<InvoiceForm> {
   String invoiceNumber;
+  RODatabase roDatabase;
 
+  SharedPreferences sharedPreferences;
+  String roCode;
+
+  initSharedPrefs() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      roCode = sharedPreferences.getString('ro_code');
+    });
+  }
+
+  initRODatabase() async {
+    _getRODatabase(roCode).then((value) {
+      _roList = value;
+      for (int i = 0; i < _roList.length; i++) {
+        if (_roList[i].ro_code == roCode) {
+          roDatabase = _roList[i];
+          print('RO ---> ' + roDatabase.toString());
+        }
+      }
+      setState(() {
+        invoiceNumber = roDatabase.invoice_number;
+      });
+    });
+  }
 
   @override
-  initState(){
-    invoiceNumber='Not Found';
-
-    RODatabase roDatabase = new RODatabase();
-    roDatabase.ro_code='12345';
-    roDatabase.invoice_number='12';
-    roDatabase.purchase_number='34';
-    roDatabase.save();
+  initState() {
+    invoiceNumber = 'N/A';
+    initSharedPrefs();
+    initRODatabase();
     super.initState();
   }
 
   TextEditingController contactNameController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
-          new Text('Invoice #'+invoiceNumber),
+          new Text('Invoice #' + invoiceNumber),
           new TextFormField(
             controller: contactNameController,
             decoration: new InputDecoration(labelText: 'Contact Name'),
@@ -44,13 +69,12 @@ class _InvoiceFormState extends State<InvoiceForm> {
 
               Map results =
                   await Navigator.of(context).push(new MaterialPageRoute(
-                  builder: (context) => ContactScreen(
-                    contactType: 'Customer',
-                    isAdmin: false,
-                  )));
+                      builder: (context) => ContactScreen(
+                            contactType: 'Customer',
+                            isAdmin: false,
+                          )));
 
-              if (results != null &&
-                  results.containsKey('contact_selection')) {
+              if (results != null && results.containsKey('contact_selection')) {
                 setState(() {
                   contactNameController.text = results['contact_selection'];
                 });
@@ -61,5 +85,25 @@ class _InvoiceFormState extends State<InvoiceForm> {
         ],
       ),
     );
+  }
+
+  List<RODatabase> _roList = [];
+
+  Future<List<RODatabase>> _getRODatabase(String roCode) async {
+    List<RODatabase> roDatabaseList;
+    QueryBuilder<ParseObject> queryBuilder;
+    queryBuilder = QueryBuilder<RODatabase>(RODatabase());
+    ParseResponse apiResponse = await queryBuilder.query();
+    if (apiResponse.success && apiResponse.result != null) {
+      final List<ParseObject> listFromApi = apiResponse.result;
+      roDatabaseList = new List();
+      for (int i = 0; i < listFromApi.length; i++) {
+        Map output = json.decode(listFromApi[i].toString());
+        RODatabase roDatabase = new RODatabase().fromJson(output);
+        print(roDatabase.toString());
+        roDatabaseList.add(roDatabase);
+      }
+    }
+    return roDatabaseList;
   }
 }
