871259636cf4ce3a53c556c50d1b97ccb92f4342
Obli Nishanth Oblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-05-12 14:03:07 +0530

    RODatabase started for receiving Invoice Number and Purchase Number

--- lib/Forms/InvoiceForm.dart
+++ lib/Forms/InvoiceForm.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hello_world/ContactScreen.dart';
+import 'package:hello_world/DataClasses/RODatabase.dart';
 
 class InvoiceForm extends StatefulWidget {
   @override
@@ -9,13 +10,28 @@ class InvoiceForm extends StatefulWidget {
 
 
 class _InvoiceFormState extends State<InvoiceForm> {
+  String invoiceNumber;
+
+
+  @override
+  initState(){
+    invoiceNumber='Not Found';
+
+    RODatabase roDatabase = new RODatabase();
+    roDatabase.ro_code='12345';
+    roDatabase.invoice_number='12';
+    roDatabase.purchase_number='34';
+    roDatabase.save();
+    super.initState();
+  }
+
   TextEditingController contactNameController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
-          new Text('New Invoice'),
+          new Text('Invoice #'+invoiceNumber),
           new TextFormField(
             controller: contactNameController,
             decoration: new InputDecoration(labelText: 'Contact Name'),
