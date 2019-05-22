cd253a006d448782e1db759f8a60161b0b7f2d9d
nishanthoblirajan <obli.oblirajan.13@ucl.ac.uk>
2019-05-22 18:39:23 +0530

    ledgerview working

--- lib/LedgerView.dart
+++ lib/LedgerView.dart
@@ -1,27 +1,30 @@
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:hello_world/DataClasses/ContactDatabase.dart';
 import 'package:hello_world/DataClasses/LedgerDatabase.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LedgerView extends StatefulWidget {
   String ledgerID;
+
   @override
   _LedgerViewState createState() => _LedgerViewState();
 
-  LedgerView({Key key, @required this.ledgerID}):super(key:key);
+  LedgerView({Key key, @required this.ledgerID}) : super(key: key);
 }
 
 class _LedgerViewState extends State<LedgerView> {
-  String ledgerName = '';
+  ContactDatabase contactDatabase;
   String ledgerID = '';
+  String ledgerName = '';
 
   @override
   initState() {
     initSharedPrefs();
     ledgerID = widget.ledgerID;
+    getContactFromID(ledgerID);
     super.initState();
   }
 
@@ -34,6 +37,7 @@ class _LedgerViewState extends State<LedgerView> {
       roCode = sharedPreferences.getString('ro_code');
     });
   }
+
   @override
   Widget build(BuildContext context) {
     var futurebuilder = new FutureBuilder(
@@ -54,8 +58,7 @@ class _LedgerViewState extends State<LedgerView> {
           );
         }
       },
-      future:
-      _query(roCode, ledgerID),
+      future: _query(roCode, ledgerID),
     );
 
     return Scaffold(
@@ -74,12 +77,27 @@ class _LedgerViewState extends State<LedgerView> {
       ),
     );
   }
+
+  void buildAppBarTitle() {
+    if (contactDatabase != null) {
+      setState(() {
+        ledgerName = 'Ledger - ${contactDatabase.contact_name}';
+      });
+    } else {
+      setState(() {
+        ledgerName = '';
+      });
+    }
+  }
+
   _query(String roCode, String ledgerID) async {
     QueryBuilder<ParseObject> queryBuilder;
-      queryBuilder = QueryBuilder<LedgerDatabase>(LedgerDatabase())
-        ..whereEqualTo(LedgerDatabase.roCode, roCode)..whereEqualTo(LedgerDatabase.ledgerID, ledgerID);
+    queryBuilder = QueryBuilder<LedgerDatabase>(LedgerDatabase())
+      ..whereEqualTo(LedgerDatabase.roCode, roCode)
+      ..whereEqualTo(LedgerDatabase.ledgerID, ledgerID);
     return queryBuilder.query();
   }
+
   List<LedgerDatabase> _ledgerDatabase = [];
 
   List<Widget> _getData(AsyncSnapshot snapshot) {
@@ -90,8 +108,7 @@ class _LedgerViewState extends State<LedgerView> {
       for (int i = 0; i < listFromApi.length; i++) {
         print(listFromApi[i].toString());
         Map output = json.decode(listFromApi[i].toString());
-        LedgerDatabase ledgerDatabase =
-        new LedgerDatabase().fromJson(output);
+        LedgerDatabase ledgerDatabase = new LedgerDatabase().fromJson(output);
         print('ledger description : ${ledgerDatabase.ledger_description}');
         _ledgerDatabase.add(ledgerDatabase);
       }
@@ -105,5 +122,21 @@ class _LedgerViewState extends State<LedgerView> {
       ));
     }
     return widgetLists;
+  }
+
+  Future<void> getContactFromID(String objectID) async {
+    ParseResponse apiResponse = await ContactDatabase().getObject(objectID);
+
+    if (apiResponse.success && apiResponse.count > 0) {
+      setState(() {
+        contactDatabase = apiResponse.result;
+        print('ledgerName $contactDatabase');
+      });
+      buildAppBarTitle();
+      return contactDatabase;
+    } else {
+      print(keyAppName + ': ' + apiResponse.error.message);
+      return '';
+    }
   }
 }
